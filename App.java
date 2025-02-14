43e4da5fb64a680e1f83ac683a61331944284ba3
PedroGabrielGuimaraesFernandes <36249685+PedroGabrielGuimaraesFernandes@users.noreply.github.com>
2025-02-14 11:55:44 +0000

    Aula 1

---
+++ App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App{
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Informe seu nome:");
+        String nome = entrada.nextLine();
+        System.out.println("Bem-vindo "+ nome +" à disciplina de Estrutura de Dados!");
+        entrada.close();
+
+    }
+}
